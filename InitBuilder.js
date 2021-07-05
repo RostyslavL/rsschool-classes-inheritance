a38625ee88fc2d23a03b9aa8a110d4729f7705bd
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-05 23:23:37 +0200

    fix: Provided PR suggestions corrections

--- InitBuilder.js
+++ InitBuilder.js
@@ -1,79 +1,91 @@
+"use strict";
 
-/*
-new IntBuilder(int) // constructor takes starting integer, if not passed starts with 0;
-.plus(...n)         // take infinite number of integers and sum all with stored value;
-.minus(...n)        // take infinite number of integers and subtract from stored value; 
-.multiply(n)        // multiply param n on stored value;
-.divide(n)          // leaves integer part of division stored value on n; 
-.mod(n)             // leaves remainder of the division stored value with on n;
-.get()              // returns stored value;
-*/
-"use strict"
-
-class  baseBilder{  // 
+class  BaseBuilder{  // 
     constructor(value){
-        this.value = value 
+      this.value = value;
     }
     plus(...n) {
-        this.value =  this.value  + n.reduce((sum, value) => sum + value) // this
-        return this
+      this.value =  this.value  + n.reduce((sum, value) => sum + value); // this
+      return this;
     }
-    divide(n){
-        this.value = this.value / n
-        return this // this
-    }
-
-    get storedValue() { 
-        return this.value
+    get() { 
+      this.value = this.value;
+      return this;
     }
 }
 
-class IntBuilder extends baseBilder{
+class IntBuilder extends BaseBuilder{
     constructor(value){
-        value = typeof value === 'number' ? value : 0
-        super(value)
+      value = typeof value === 'number' ? value : 0;
+      super(value);
     }
     multiply(n){
-        this.value = this.value * n
-        return this // this
+      this.value = this.value * n;
+      return this;
+      
     }
     divide(n){
-        this.value = this.value / n
-        return this // this
+      this.value = this.value / n;
+      return this;
     }
     mod(n){
-        this.value = this.value % n
-        return this // this
+      this.value = this.value % n;
+      return this;
     }
     minus(...n){
-        this.value = this.value - n.reduce((dif, value) => dif + value)
-        return this // this
+      this.value = this.value - n.reduce((dif, value) => dif + value);
+      return this;
     }
-    multiply(n){
-        this.value = this.value * n
-        return this 
-    }
-    divide(n){
-        this.value = this.value / n
-        return this // this
-    }
-    mod(n){
-        this.value = this.value % n
-        return this // this
-    }
-    static random(from, to) {
-      let min = Math.ceil(from)
-      let max = Math.floor(to)
-      return Math.floor(Math.random() * (max - min)) + min
+    static random(from, to){
+      let min = Math.ceil(from);
+      let max = Math.floor(to);
+      return Math.floor(Math.random() * (max - min)) + min;
     }
 }
 
-
-let intBuilder = new IntBuilder(10)
-console.log(intBuilder.plus(2, 3, 2)) //17
-console.log(intBuilder.minus(1, 2)) //14
-console.log(intBuilder.multiply(2)) //28
-console.log(intBuilder.divide(4)) //7
-console.log(intBuilder.mod(3))//1
-console.log(intBuilder.storedValue); // get method
+let intBuilder = new IntBuilder(10);
+console.log(intBuilder.plus(2, 3, 2)); //17
+console.log(intBuilder.minus(1, 2)); //14
+console.log(intBuilder.multiply(2)); //28
+console.log(intBuilder.divide(4)); //7
+console.log(intBuilder.mod(3));//1
+console.log(intBuilder.get()); // get method
 console.log(IntBuilder.random(10, 100)); // static method
+
+function StringBuilder(value){
+  value = typeof value === 'string' ? value : '';
+  Object.assign(this, new BaseBuilder(value));
+}
+
+StringBuilder.prototype = Object.create(BaseBuilder.prototype);
+
+StringBuilder.prototype.multiply = function(n){
+    this.value = StringBuilder.value.repeat(n);
+    return this;
+  }
+  StringBuilder.prototype.minus = function(n){
+    this.value = this.value.slice(0, -n);
+    return this;
+  }
+  StringBuilder.prototype.divide = function(n){
+    this.value = this.value.slice(0, n);
+    return this;
+  }
+  StringBuilder.prototype.remove = function(string){
+    this.value = this.value.replace(string, '');
+    return this;
+  }
+  StringBuilder.prototype.sub = function(length, n){
+    this.value = this.value.substring(length, n);
+    return this;
+  }
+
+let strBuilder = new StringBuilder('Test');
+
+console.log(strBuilder.plus('adasa', '!5678'));
+console.log(strBuilder.minus(3));
+// console.log(StringBuilder.multiply(3));
+console.log(strBuilder.divide(4));
+console.log(strBuilder.remove('s'));
+console.log(strBuilder.sub(3, 1));
+// console.log(strBuilder.get());
